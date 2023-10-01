61523e106a1fdfcf1e65f1325d84720c5742d3f3
Ismail0v12 <orifisamilov12@mail.ru>
2023-10-01 16:49:42 +0500

    added stylelint and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:i18next/recommended"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      2,
+      2
+    ],
+    "i18next/no-literal-string": ["warn", {markupOnly: true}],
+    "@typescript-eslint/no-unused-vars": [1],
+    "react/react-in-jsx-scope": [0],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
